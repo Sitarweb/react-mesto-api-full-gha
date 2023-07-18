c653517e0de329b921a5752150be1828187285d7
Simon Tarasenko <semeontarasenko@yandex.ru>
2023-07-18 19:19:10 +0300

    Добавил пакет cors

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { signup, signin } = require('./middlewares/validation');
@@ -12,6 +13,7 @@ const errorHandler = require('./middlewares/error-handler');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
+app.use(cors());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
